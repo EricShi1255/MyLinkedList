f6022d353c9fa6fbf20d8441fa7e9ccc8ff1fc83
EricShi1255 <eshi20@stuy.edu>
2021-01-07 12:59:24 -0500

    fixed part i except for add at index

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -7,9 +7,9 @@ public class MyLinkedList{
     }
     public int size() {
         int count = 0;
-        curr = start;
+        Node curr = start;
         while (curr != null) {
-            curr = curr.next;
+            curr = curr.next();
             count++;
         }
         return count;
@@ -17,16 +17,23 @@ public class MyLinkedList{
     public boolean add(String value) {
         //adds to end of list;
         Node added = new Node(value);
-        end.next() = added;
-        end = added;
+        if (start == null) {
+            start = added;
+            end = added;
+        }
+        else {
+            end.setNext(added);
+            end = added;
+        }
+        return true;
     }
     public void add(int index, String value) {
-
+        //to do
     }
     public String get(int index) {
         int pos = 0;
         String result = "";
-        curr = start;
+        Node curr = start;
         while (curr != null) {
             if (pos == index) {
                result =  curr.value();
@@ -36,10 +43,30 @@ public class MyLinkedList{
         return result;
     }
     public String set(int index, String value) {
-
+        int pos = 0;
+        String result = "";
+        Node curr = start;
+        while (curr != null) {
+            if (pos == index) {
+               result =  curr.value(); //value popped out
+               curr.setValue(value); //value inserted
+            }
+            curr = curr.next();
+            pos++;
+        }
+        return result;
     }
     public String toString() {
-        
+        String result = "[";
+        Node curr = start;
+        while (curr != null) {
+            result += curr.value();
+            if (curr.next() != null) {
+                result += ", ";
+            }
+            curr = curr.next();
+        }
+        return(result + "]");
     }
  //Any helper method that returns a Node object MUST BE PRIVATE!
 }
